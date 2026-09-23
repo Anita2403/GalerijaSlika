30e8fb171cfcbc1c84da438794a8ed5cfcbbeb62
Anita <sarcevic.anita@gmail.com>
2026-09-23 20:55:29 +0200

    dodavanje datoteka, ......

---
+++ src/services/slike/SlikaPodaci.js
@@ -0,0 +1,39 @@
+export const slike = [
+    {
+        sifra: 1,
+        naziv: 'Purle serenity',
+        godinaIzrade: 2026,
+        tehnika: 'mix media na medijapanu',
+        sirina: 50,
+        visina: 70,
+        dostupnost: true,
+        cijena: 200, 
+        image: '',
+    },
+    {
+        sifra: 2,
+        naziv: '',
+        godinaIzrade: 2026,
+        tehnika: 'akril na platnu',
+        sirina: 20,
+        visina: 30,
+        dostupnost: false,
+        cijena: 50,
+        image: '',
+    },
+    {
+        sifra: 3,
+        naziv: '',
+        godinaIzrade: 2026,
+        tehnika: '',
+        sirina: 50,
+        visina:70,
+        dostupnost: false,
+        cijena: 50,
+        image: '',
+    },
+    {
+        sifra: 
+    }
+
+]
